380119898a4b604748d58c54744a846a627c32b4
Maciej Janiszewski <janiszewski_maciej@outlook.com>
2019-07-21 16:33:52 +0200

    Fix sensor readings

--- libband/sensors.py
+++ libband/sensors.py
@@ -1,3 +1,4 @@
+import binascii
 from enum import IntEnum
 import struct
 
@@ -25,6 +26,8 @@ class Sensor(IntEnum):
     Barometer = 58
     Elevation = 71
     CaloriesWithDailyValues = 107
+    DistanceWithDailyValues = 108
+    PedometerWithDailyValues = 109
     ElevationWithDailyValues = 110
     UVWithDailyValues = 111
     AmbientLightWithDailyValues = 112
@@ -32,27 +35,52 @@ class Sensor(IntEnum):
     LogEntry = 124
 
 
+class MotionType(IntEnum):
+    Unknown = 0
+    Idle = 1
+    Walking = 2
+    Jogging = 3
+    Running = 4
+
+
+class UVIndexLevel(IntEnum):
+    NoUV = 0
+    Low = 1
+    Medium = 2
+    High = 3
+    VeryHigh = 4
+
+
 class SensorReading():
     subscription_type = Sensor.Unknown
     packet_length = 0
     missed_samples = 0
     sample_size = 0
+    _packet = b''
+
+    def get_value_verbose(self):
+        return binascii.hexlify(self._packet)
 
     def __str__(self):
-        return '%s Value: %s' % (
-            self.subscription_type, self.value)
+        return f'{self.subscription_type}: Value: {self.get_value_verbose()}'
 
     @property
     def header_length(self):
         return 4 + 1 + 1 + 2
 
     def decode_packet(self, packet):
-        self.packet_length, self.missed_samples, self.sample_size = \
-            struct.unpack("LxBH", packet[:self.header_length])
+        self._packet = packet
+        (
+            self.packet_length,
+            self.subscription_type,
+            self.missed_samples,
+            self.sample_size
+        ) = struct.unpack("LBBH", packet[:self.header_length])
 
 
 class DeviceContactSensor(SensorReading):
-    subscription_type = Sensor.DeviceContact
+    def get_value_verbose(self):
+        return 'Wearing' if self.value else 'Not wearing'
 
     def decode_packet(self, packet):
         super().decode_packet(packet)
@@ -60,38 +88,189 @@ class DeviceContactSensor(SensorReading):
 
 
 class PedometerSensor(SensorReading):
-    subscription_type = Sensor.Pedometer
+    def get_value_verbose(self):
+        return f'{self.value} steps'
 
     def decode_packet(self, packet):
         super().decode_packet(packet)
-        self.value = struct.unpack("L", packet[
-            self.header_length:self.header_length + self.packet_length])
+        self.value = struct.unpack(
+            "L", packet[self.header_length:self.header_length + 4]
+        )[0]
+
+
+class PedometerSensorWithDailyValues(PedometerSensor):
+    def get_value_verbose(self):
+        return f'{self.value} steps ({self.value_today} today)'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value, self.value_today = struct.unpack(
+            "LL", packet[self.header_length:self.header_length + 8]
+        )
 
 
 class HeartRateSensor(SensorReading):
-    subscription_type = Sensor.HeartRate
     quality = False
+
+    def get_value_verbose(self):
+        return f'{self.value} ({"Locked" if self.quality else "Acquiring"})'
 
     def decode_packet(self, packet):
         super().decode_packet(packet)
-        self.value = packet[self.header_length + 1]
-        self.quality = packet[self.header_length + 2] >= 6
+        self.value, self.quality = struct.unpack(
+            'BB', packet[self.header_length:]
+        )
+        self.quality = self.quality >= 6
 
 
 class BatteryGaugeSensor(SensorReading):
-    subscription_type = Sensor.BatteryGauge
     filtered_voltage = 0
     battery_gauge_alerts = 0
 
+    def get_value_verbose(self):
+        return f'{self.value}%'
+
     def decode_packet(self, packet):
         super().decode_packet(packet)
-        beginning = self.header_length
-        self.filtered_voltage = struct.unpack(
-            "H", packet[beginning + 1:beginning + 2])
-        self.battery_gauge_alerts = struct.unpack(
-            "H", packet[beginning + 2:beginning + 4])
-        self.value = (packet[beginning + 1] / 10) * 10
-        self.value = 100 if self.value > 100 else self.value
+        self.value, self.filtered_voltage, self.battery_gauge_alerts = (
+            struct.unpack('<BHH', packet[self.header_length:])
+        )
+
+
+class SkinTemperatureSensor(SensorReading):
+    def get_value_verbose(self):
+        return self.value
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        decoded = struct.unpack(f'h{"x"*8}', packet[self.header_length:])[0]
+        self.value = decoded / 100.0
+
+
+class DistanceSensor(SensorReading):
+    speed = 0
+    pace = 0
+    current_motion = MotionType.Idle
+
+    def get_value_verbose(self):
+        return f'{self.value} total. Speed: {self.speed}. Pace: {self.pace}'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value, self.speed, self.pace = (
+            struct.unpack(f'L{"x"*8}LLxx', packet[self.header_length:])
+        )
+
+
+class DistanceSensorWithDailyValues(DistanceSensor):
+    value_today = 0
+    current_motion = MotionType.Idle
+
+    def get_value_verbose(self):
+        return (
+            f'{self.value} total ({self.value_today} today). '
+            f'Speed: {self.speed}. Pace: {self.pace}. '
+            f'Current Motion: {self.current_motion}'
+        )
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        (
+            self.value,
+            self.speed,
+            self.pace,
+            self.current_motion,
+            self.value_today
+        ) = struct.unpack(f'LLLBL', packet[self.header_length:])
+        self.current_motion = MotionType(self.current_motion)
+
+
+class AccelerometerSensor(SensorReading):
+    def get_value_verbose(self):
+        return (
+            f'ax: {self.acceleration_x}, ay: {self.acceleration_y} '
+            f'az: {self.acceleration_z}'
+        )
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        ax, ay, az = struct.unpack(
+            'hhh',
+            packet[self.header_length:self.header_length+6]
+        )
+        self.acceleration_x = ax * 0.000244140625
+        self.acceleration_y = ay * 0.000244140625
+        self.acceleration_z = az * 0.000244140625
+
+
+class AccelerometerGyroscopeSensor(AccelerometerSensor):
+    def get_value_verbose(self):
+        return (
+            f'{super().get_value_verbose()} '
+            f'vx: {self.velocity_x}, vy: {self.velocity_y} '
+            f'vz: {self.velocity_z}'
+        )
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        vx, vy, vz = struct.unpack(
+            'hhh',
+            packet[self.header_length+6:]
+        )
+        self.velocity_x = vx * 0.030487804878048783
+        self.velocity_y = vy * 0.030487804878048783
+        self.velocity_z = vz * 0.030487804878048783
+
+
+class UVSensor(SensorReading):
+    def get_value_verbose(self):
+        return f'{self.value.name}'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value = UVIndexLevel(
+            struct.unpack('xxHxx', packet[self.header_length:])[0]
+        )
+
+
+class AmbientLightSensor(SensorReading):
+    def get_value_verbose(self):
+        return f'{self.value}'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value = struct.unpack('Hxxxx', packet[self.header_length:])[0]
+
+
+class Calorie1SSensor(SensorReading):
+    def get_value_verbose(self):
+        return f'{self.value} cal'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value = struct.unpack(
+            f'I{"x"*16}', packet[self.header_length:]
+        )[0]
+
+
+class RRIntervalSensor(SensorReading):
+    def get_value_verbose(self):
+        return f'{self.value}'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value = struct.unpack(
+            f'xxxxH', packet[self.header_length:]
+        )[0] * 0.016592
+
+
+class GsrSensor(SensorReading):
+    def get_value_verbose(self):
+        return f'{self.value}'
+
+    def decode_packet(self, packet):
+        super().decode_packet(packet)
+        self.value = struct.unpack('<xIxx', packet[self.header_length:])
 
 
 def decode_sensor_reading(packet):
@@ -99,16 +278,40 @@ def decode_sensor_reading(packet):
     Detect which sensor reading we got and decode it
     """
     subscription_type = packet[6]
-
-    if subscription_type == Sensor.HeartRate:
-        sensor = HeartRateSensor()
-    elif subscription_type == Sensor.Pedometer:
-        sensor = PedometerSensor()
-    elif subscription_type == Sensor.DeviceContact:
-        sensor = DeviceContactSensor()
-    elif subscription_type == Sensor.BatteryGauge:
-        sensor = BatteryGaugeSensor()
-    else:
-        sensor = SensorReading()
-    sensor.decode_packet(packet[2:])
+    sensor = {
+        Sensor.Accelerometer128MS: AccelerometerSensor,
+        Sensor.Accelerometer32MS: AccelerometerSensor,
+        Sensor.AccelerometerGyroscope128MS: AccelerometerGyroscopeSensor,
+        Sensor.AccelerometerGyroscope32MS: AccelerometerGyroscopeSensor,
+        Sensor.Distance: DistanceSensor,
+        Sensor.Gsr: GsrSensor,
+        Sensor.HeartRate: HeartRateSensor,
+        Sensor.Pedometer: PedometerSensor,
+        Sensor.SkinTemperature: SkinTemperatureSensor,
+        Sensor.UV: UVSensor,
+        Sensor.AmbientLight: AmbientLightSensor,
+        Sensor.DeviceContact: DeviceContactSensor,
+        Sensor.BatteryGauge: BatteryGaugeSensor,
+        Sensor.UVFast: UVSensor,
+        Sensor.Calories1S: Calorie1SSensor,
+        Sensor.Accelerometer16MS: AccelerometerSensor,
+        Sensor.AccelerometerGyroscope16MS: AccelerometerGyroscopeSensor,
+        # MSFT Band 2 Only
+        Sensor.RRInterval: RRIntervalSensor,
+        # TODO: Barometer
+        # TODO: Elevation
+        # TODO: Calories With Daily Values
+        Sensor.DistanceWithDailyValues: DistanceSensorWithDailyValues,
+        Sensor.PedometerWithDailyValues: PedometerSensorWithDailyValues,
+        # TODO: Elevation With Daily Values
+        # TODO: UV With Daily Values
+        # TODO: Ambient Light With Daily Values
+        # TODO: Gsr200MS
+        # TODO: LogEntry (maybe this one is only on non-production devices?)
+    }.get(subscription_type, SensorReading)()
+    try:
+        sensor.decode_packet(packet[2:])
+    except Exception as exc:
+        print(binascii.hexlify(packet[2:]))
+        print(exc)
     return sensor
